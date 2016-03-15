c803e103b8a783b8ea4ebdeaa44b8cbececfe693
Jeffrey Wong <jwong@dayspring-tech.com>
2016-03-15 14:12:54 -0700

    use built-in form_login authentication

--- symfony/src/Dayspring/SecurityBundle/Controller/SecurityController.php
+++ symfony/src/Dayspring/SecurityBundle/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace Dayspring\SecurityBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,26 +11,33 @@ class SecurityController extends Controller
 {
 
     /**
-     * @Route("/login", name="security_login")
+     * @Route("/login", name="_login")
+     * @Template
      */
     public function loginAction()
     {
         $helper = $this->get('security.authentication_utils');
 
-        return $this->render('DayspringSecurityBundle:Security:login.html.twig', array(
+        return array(
             // last username entered by the user (if any)
             'last_username' => $helper->getLastUsername(),
             // last authentication error (if any)
             'error' => $helper->getLastAuthenticationError(),
-        ));
+        );
     }
 
     /**
-     * @Route("/login_check", name="security_login_check")
+     * @Route("/_login_check", name="_login_check")
      */
     public function loginCheckAction()
     {
         // will never be executed
     }
 
+    /**
+     * @Route("/logout", name="_logout")
+     */
+    public function logoutAction(Request $request)
+    {
+    }
 }
