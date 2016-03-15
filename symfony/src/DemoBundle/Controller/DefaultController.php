b63b00585f05cf628d261667d3588ac85324c264
Jeffrey Wong <jwong@dayspring-tech.com>
2016-03-15 16:36:13 -0700

    fix example of securing controller action

--- symfony/src/DemoBundle/Controller/DefaultController.php
+++ symfony/src/DemoBundle/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace DemoBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use JMS\SecurityExtraBundle\Annotation\Secure;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
@@ -21,7 +21,7 @@ class DefaultController extends Controller
     /**
      * @Route("/secure", name="demo_secure")
      * @Template()
-     * @Secure(roles="IS_AUTHENTICATED_FULLY")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
      */
     public function secureAction()
     {
