91d78ca228a925f32568375c489432cca44551c1
Jeffrey Wong <jwong@dayspring-tech.com>
2017-03-09 16:23:23 -0800

    update login bundle

--- symfony/app/propel/fixtures/01_users.sql
+++ symfony/app/propel/fixtures/01_users.sql
@@ -1,14 +1,18 @@
 INSERT INTO `roles`
 (`id`, `role_name`)
 VALUES
-(1, 'ROLE_User');
+(1, 'ROLE_User'),
+(2, 'ROLE_Admin');
 
 INSERT INTO `users`
 (`id`, `email`, `password`, `salt`)
 VALUES
-(1, 'testuser@example.com', '$2y$12$nOQ1p5XXnnFCOn5NEC8B3ez05hYSuOq1ka9SrMbxNpKZF8/BjiamG', null);
+(1, 'admin@example.com', '$2y$12$nOQ1p5XXnnFCOn5NEC8B3ez05hYSuOq1ka9SrMbxNpKZF8/BjiamG', null),
+(2, 'testuser@example.com', '$2y$12$nOQ1p5XXnnFCOn5NEC8B3ez05hYSuOq1ka9SrMbxNpKZF8/BjiamG', null);
 
 INSERT INTO `roles_users`
 (`user_id`, `role_id`)
 VALUES
-(1, 1);
+(1, 1),
+(1, 2),
+(2, 1);
