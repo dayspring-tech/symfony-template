a89a165b04d2b65f547aea6e3396079d3ec670e5
Jeffrey Wong <jwong@dayspring-tech.com>
2019-07-19 16:58:10 -0700

    add debug for failing test

--- symfony/src/DemoBundle/Tests/Controller/DefaultControllerTest.php
+++ symfony/src/DemoBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,6 +10,7 @@ namespace DemoBundle\Tests\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use function var_dump;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -19,7 +20,7 @@ class DefaultControllerTest extends WebTestCase
         $client = self::createClient();
 
         $crawler = $client->request("GET", "/_demo/");
-
+var_dump($client->getResponse()->getContent());
         $this->assertGreaterThan(
             0,
             $crawler->filter('html:contains("Theme example")')->count()
